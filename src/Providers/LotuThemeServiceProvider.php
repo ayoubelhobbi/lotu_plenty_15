ddb6d7b2c13bdc546107ff44d75b6015fffeccb1
Ayoub <ayoubelhobbi@gmail.com>
2018-09-20 17:41:19 +0200

    compatibility

--- src/Providers/LotuThemeServiceProvider.php
+++ src/Providers/LotuThemeServiceProvider.php
@@ -7,6 +7,7 @@ use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
+use IO\Helper\TemplateContainer;
 
 /**
  * Class LotuThemeServiceProvider
